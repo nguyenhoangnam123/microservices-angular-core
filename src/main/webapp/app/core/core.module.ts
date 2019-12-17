9b85025bf78c429da3b4e990b580fa57f4538d74
Nam <namnh21894@gmail.com>
2019-12-17 09:45:53 +0700

    add _base and reducers

--- src/main/webapp/app/core/core.module.ts
+++ src/main/webapp/app/core/core.module.ts
@@ -76,7 +76,7 @@ import { fontAwesomeIcons } from './icons/font-awesome-icons';
     }
   ]
 })
-export class EGpCoreModule {
+export class CoreModule {
   constructor(iconLibrary: FaIconLibrary, dpConfig: NgbDatepickerConfig, languageService: JhiLanguageService) {
     registerLocaleData(locale);
     iconLibrary.addIconPacks(fas);
