16cf8c73eb1a12dd31ae1c0262dfe4abf39c8a98
Nam <namnh21894@gmail.com>
2019-12-29 22:55:37 +0700

    import css dependency

--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -1,6 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { ProdConfig } from './blocks/config/prod.config';
 import { EGpAppModule } from './app.module';
+import 'content/plugins/flaticon/flaticon.css';
+import 'content/plugins/flaticon2/flaticon.css';
+import 'content/plugins/flaticon/flaticon.css';
+import 'content/plugins/flaticon2/flaticon.css';
+import 'content/plugins/line-awesome/css/line-awesome.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'socicon/css/socicon.css';
+import '../content/scss/styles.scss';
 
 ProdConfig();
 
